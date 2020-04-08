65076980a9be4d51b24d7bf2816d68ffc7bc92ce
Bella Zhan <bellazhan2021@u.northwestern.edu>
2020-04-08 15:41:35 -0500

    fixed token error

---
+++ src/shared/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7ZbUzhyIOwPyY6PcOqcJSVkZXSmAOBK8",
+    authDomain: "quick-react-c282d.firebaseapp.com",
+    databaseURL: "https://quick-react-c282d.firebaseio.com",
+    projectId: "quick-react-c282d",
+    storageBucket: "quick-react-c282d.appspot.com",
+    messagingSenderId: "32319431406",
+    appId: "1:32319431406:web:07e55bcf84b8f0903009ff",
+    measurementId: "G-44ZVJL03MY"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase;
